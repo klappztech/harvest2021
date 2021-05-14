3ddc7596482c5442295ed226806e5e6b1ef5f37a
klappztech <klappztech@gmail.com>
2021-05-14 10:40:47 +0530

    initialcommit

---
+++ notes.php
@@ -0,0 +1,29 @@
+
+
+INDEX {
+    <!-- Send Us an Enquiry - form not working -->
+    <!-- services description -->
+    <!-- Social media handles -->
+}
+
+ABOUT {
+    <!-- OK -->
+}
+
+SERVICES {
+    <!-- services description -->
+    <!-- What We Offer For Your Dream House - boxes -->
+}
+
+PROJECTS {
+    <!-- OK -->
+}
+
+PRICING {
+ <!-- paragraph --> 
+}
+
+CONTACT {
+    <!-- map -->
+    <!-- form -->
+}
